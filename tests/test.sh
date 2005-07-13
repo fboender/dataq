b44b5e5f5ca37c97053d0c7f0ff16ea8a7fc5d02
todsah <todsah@5e763a1a-bcdf-427f-adeb-eebfbc5de0a3>
2005-07-13 18:32:09 +0000

    DataQ server exiting with an error is now handled.
    git-svn-id: https://svn.electricmonk.nl/svn/dataq/trunk@25 5e763a1a-bcdf-427f-adeb-eebfbc5de0a3

--- tests/test.sh
+++ tests/test.sh
@@ -3,6 +3,12 @@
 for TEST in `ls -1 -d test*| grep -v 'test.sh'`; do
 	echo -n "Running test in $TEST... "
 	../src/dataq.py -dV -c $TEST/dataq.xml > dataq.out
+	
+	if [ "$?" -ne 0 ]; then
+		echo "DataQ server returned an error. Please check that is correctly working. Aborting the testrun."
+		exit
+	fi
+		
 
 	TEMP=`grep 'PID:' dataq.out`
 	PID=`echo $TEMP | cut -d':' -f2`
